1dc0711c7274570bb00571a9c21ba45cd8085697
andrewcr90 <andrewcrodriguez@gmail.com>
2016-06-14 18:30:16 -0500

    cambios interfaz
    cambio componente asignacion de permisos
    cambios en botones de busqueda

--- modules/sharingSetting/sharingSettingController.js
+++ modules/sharingSetting/sharingSettingController.js
@@ -7,8 +7,9 @@
 						'commonvariable',
 						'sharingSetting',
 						'dhisResource',
+						'commonvariable',
 						function($scope, $filter, commonvariable,
-								sharingSetting, dhisResource) {
+								sharingSetting, dhisResource, commonvariable) {
 
 							// variables
 							var $translate = $filter('translate');
@@ -16,10 +17,13 @@
 							$scope.objectSelected = [];
 							$scope.changeButton = 0;
 							$scope.ugStatus = [ 0, 0, 0, 0 ];
+							$scope.statusChange = [ 0, 0, 0, 0 ];
 							$scope.ugStatusAccess = [ 0, 0, 0, 0 ];
 							$scope.ugStatusAccessExternal = [ 0, 0, 0, 0 ];
+							$scope.objectSele = [];
 							$scope.publicAccess;
 							$scope.externalAccess;
+
 							// /Object array of api object and type variable in
 							// sharing Setting resource
 							$scope.elements = [
@@ -91,6 +95,12 @@
 								$scope.currentObject = object;
 								$scope.type = object.type;
 								$scope.page = object.page;
+								// $scope.object.name =
+								// (commonvariable.OptionSetSelected.departamento
+								// != undefined) ?
+								// commonvariable.OptionSetSelected.departamento.code
+								// : "";
+
 								dhisResource
 										.GET({
 											resource : object.resource,
@@ -99,6 +109,12 @@
 										}).$promise
 										.then(function(response) {
 											$scope.mObjects = response[object.resource];
+											for (a = 0; a < $scope.mObjects.length; a++) {
+												$scope.mObjects[a]["status"] = "1";
+												$scope.mObjects[a]["key"] = a;
+											}
+											console.log("objetosssss",
+													$scope.mObjects);
 											$scope.objectToSelected = [];
 											angular
 													.forEach(
@@ -191,11 +207,11 @@
 										begin, end);
 							};
 
-// function removeRole(context) {
-// removeItem(context.id, context.name,
-// i18n_confirm_delete,
-// 'removeRole.action');
-// }
+							// function removeRole(context) {
+							// removeItem(context.id, context.name,
+							// i18n_confirm_delete,
+							// 'removeRole.action');
+							// }
 
 							// Clean Variables
 							$scope.clean = function() {
@@ -204,6 +220,7 @@
 								$scope.objectSelected = [];
 								$scope.ugStatusAccess = [ 0, 0, 0, 0 ];
 								$scope.ugStatusAccessExternal = [ 0, 0, 0, 0 ];
+								$scope.objectSele = [];
 
 							}
 
@@ -224,7 +241,8 @@
 
 								if (changeButton == 3) {
 									publicAccess = false;
-									console.log("PUBLIC ACCESS FALSE ", publicAccess)
+									console.log("PUBLIC ACCESS FALSE ",
+											publicAccess)
 
 								}
 								if (changeButton == 4) {
@@ -248,71 +266,155 @@
 							// /Get and Put the permissions to the object
 							$scope.assignPermissions = function(objectSelected) {
 								console.log(objectSelected);
-								angular.forEach($scope.objectSelected,
-										function(objectSelected) {
-											sharingSetting.get({
-												id : objectSelected,
-												type : $scope.type
-											}).$promise.then(function(result) {
-												
-												
-												console.log("ugroupsss",$scope.uGroups);
-												newShSetting = $scope.newShareObject(result,$scope.uGroups);
-												sharingSetting.POST({
-													id : objectSelected,
-													type : $scope.type
-												},newShSetting).$promise.then(function(resultPost) {
-													console.log(resultPost);
-												});	
-												console.log("RESULT", result);
-											})
-										})
-							}	
+								angular
+										.forEach(
+												$scope.objectSelected,
+												function(objectSelected) {
+													sharingSetting.get({
+														id : objectSelected,
+														type : $scope.type
+													}).$promise
+															.then(function(
+																	result) {
 
+																console
+																		.log(
+																				"ugroupsss",
+																				$scope.uGroups);
+																newShSetting = $scope
+																		.newShareObject(
+																				result,
+																				$scope.uGroups);
+																sharingSetting
+																		.POST(
+																				{
+																					id : objectSelected,
+																					type : $scope.type
+																				},
+																				newShSetting).$promise
+																		.then(function(
+																				resultPost) {
+																			console
+																					.log(resultPost);
+																		});
+																console
+																		.log(
+																				"RESULT",
+																				result);
+															})
+												})
+							}
 
-
-
-
-
-							$scope.newShareObject=function(result,uGroups){
-
+							// Assign Elements to the Api
+							$scope.newShareObject = function(result, uGroups) {
 								console.log("compare", result);
 								console.log("uGroupslength", uGroups.length);
-								var newShSetting;
-								try{
-								for (a = 0; a < result.object.userGroupAccesses.length; a++) {
-									console.log("iiiii", i);
-									for (i = 0; i < uGroups.length; i++) {
-										if ((result.object.userGroupAccesses[a].id) == (uGroups[a].id)) {
+								var newShSetting = result;
+								try {
+									for (i = 0; i < (uGroups.length) - 1; i++) {
+										console
+												.log(
+														"iiiii",
+														result.object.userGroupAccesses.length);
+
+										// for (a = 0; a <
+										// (result.object.userGroupAccesses.length)-1;
+										// a++) {
+										if ((result.object.userGroupAccesses[i].id) == (uGroups[i].id)) {
 											console.log("iguales");
-//											(uGroups).splice(0,uGroups[a]);
-											newShSetting=result;	
-//											newShSetting.object.userGroupAccesses.push(
-//													{access:access,
-//														id:uGroups[a].id}
-//													);
+											// (uGroups.object).splice(i,
+											// i);
+											console.log("ifffffff", uGroups);
+											newShSetting = result;
+											newShSetting.object.userGroupAccesses
+													.push({
+														access : access,
+														id : uGroups[i].id
+													});
 										} else {
 											console.log("Diferentes");
-											newShSetting=result;
-											newShSetting.object.userGroupAccesses.push(
-													{access:access,
-														id:uGroups[a].id}
-													);
-//											return newShSetting;
+											newShSetting = result;
+											newShSetting.object.userGroupAccesses
+													.push({
+														access : access,
+														id : uGroups[i].id
+													});
+											return newShSetting;
 										}
-										return newShSetting;
-										
+										// return newShSetting;
+
+										// }
 									}
-								}}catch(error){
-									newShSetting=result;
-									newShSetting.object["userGroupAccesses"]=[
-											{access:access,
-												id:uGroups[a].id}
-											];
+									return newShSetting;
+								} catch (error) {
+
+									newShSetting.object["userGroupAccesses"] = [ {
+										access : access,
+										id : uGroups[i].id
+									} ];
+									newShSetting.object.userGroupAccesses
+											.push({
+												access : access,
+												id : uGroups[i].id
+											});
 									return newShSetting;
 								}
-								
-								
+							}
+
+							// Get Elements selected
+							$scope.getOptionSelected = function(objectSelected,
+									mObjects) {
+								for (i = 0; i < mObjects.length - 1; i++) {
+									for (a = 0; a < objectSelected.length; a++) {
+										if ((objectSelected[a].key) == ($scope.mObjects[i].key)) {
+
+											console.log("keyyyIFFFF");
+											$scope.mObjects[i]["status"] = "2";
+
+										} else {
+											a--;
+											i++;
+										}
+									}
+									break;
+								}
+
+								// try {
+								// for (i = 0; i < objectSelected.length; i++) {
+								// if(objectSelected[i].key== mObjects[i].key){
+								//											
+								// console.log("keyyy",$scope.mObjects[i].key);
+								// }
+								// $scope.mObjects[i]["status"] = "2";
+								//
+								// // $scope.objectSele
+								// // .push({
+								// // id : objectSelected[i].id,
+								// // name : objectSelected[i].displayName,
+								// // status : "2",
+								// // });
+								//										
+								// console.log("objeto seleccionado",
+								// $scope.objectSele);
+								// console.log("mObjectsssss",
+								// $scope.mObjects);
+								// }
+								// } catch (error) {
+								// $scope.objectSelected.length = 0;
+								// }
+
+							}
+
+							// Remove Selected Elements
+							$scope.removeOptionSelected = function(mObjects) {
+								for (i = 0; i < mObjects.length - 1; i++) {
+											$scope.mObjects[i]["status"] = "1";
+										} 
+							}
+
+							// remove Group Access
+							$scope.removeGroupAccess = function(objectSelected) {
+
 							}
 
 						} ]);
